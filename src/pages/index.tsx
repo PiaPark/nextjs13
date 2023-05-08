f436d98cc4b28b8fb7f8cab3e426dc12e90cb04e
Pia <pia@protopie.io>
2023-05-08 11:38:39 +0900

    [main] 커밋 테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,6 @@ export default function Home() {
             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-            ad='test'
           >
             By{" "}
             <Image
